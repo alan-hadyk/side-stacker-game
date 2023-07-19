5e19e1cadb7278881fe02d18e784baae206918b8
alan-hadyk <alan.hadyk@gmail.com>
2023-07-19 20:43:05 +0200

    Add daisyUI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,17 @@
 import "@app/styles/global.css"
 
-export const App: React.FC = () => <h1>Side-Stacker Game</h1>
+export const App: React.FC = () => (
+  <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content text-center">
+      <div className="max-w-md">
+        <h1 className="text-5xl font-bold">Side-Stacker</h1>
+        <input
+          type="text"
+          placeholder="Enter your username"
+          className="input input-bordered input-primary w-full max-w-xs my-6"
+        />
+        <button className="btn btn-success">Get Started</button>
+      </div>
+    </div>
+  </div>
+)
