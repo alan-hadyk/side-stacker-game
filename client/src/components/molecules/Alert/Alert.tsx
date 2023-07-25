68325535691dc1d2c1e86deaf38f36a21bd558f5
alan-hadyk <alan.hadyk@gmail.com>
2023-07-25 13:58:47 +0200

    Small adjustments

--- client/src/components/molecules/Alert/Alert.tsx
+++ client/src/components/molecules/Alert/Alert.tsx
@@ -6,19 +6,13 @@ import { mapAlertTypeToStyles } from "@client/components/molecules/Alert/styles"
 
 export const Alert: React.FC<AlertProps> = ({
   children,
-  icon,
+  icon: Icon,
   type = AlertType.Default,
-}) => {
-  const Icon = icon
-
-  return (
-    <div className={`alert ${mapAlertTypeToStyles[type].container}`}>
-      {Icon && (
-        <Icon
-          className={`${mapAlertTypeToStyles[type].icon} shrink-0 w-6 h-6`}
-        />
-      )}
-      <span className="text-sm">{children}</span>
-    </div>
-  )
-}
+}) => (
+  <div className={`alert ${mapAlertTypeToStyles[type].container}`}>
+    {Icon && (
+      <Icon className={`${mapAlertTypeToStyles[type].icon} shrink-0 w-6 h-6`} />
+    )}
+    <span className="text-sm">{children}</span>
+  </div>
+)
--- client/src/containers/home/sections/activePlayers/HomeContainerActivePlayersSection.tsx
+++ client/src/containers/home/sections/activePlayers/HomeContainerActivePlayersSection.tsx
@@ -1,45 +1,21 @@
 import { useGetPlayers } from "@client/api/queries/useGetPlayers"
-import {
-  TypographyVariant,
-  TypographyWeight,
-} from "@client/components/atoms/Typography/@types/Typography"
-import { Typography } from "@client/components/atoms/Typography/Typography"
 import { AlertType } from "@client/components/molecules/Alert/@types/Alert"
 import { Alert } from "@client/components/molecules/Alert/Alert"
 import { Card } from "@client/components/molecules/Card/Card"
 import { Table } from "@client/components/organisms/Table/Table"
+import { mapPlayersToRows } from "@client/containers/home/sections/activePlayers/helpers/mapPlayersToRows"
 import { useAuthenticatedUser } from "@client/hooks/useAuthenticatedUser"
-import { PlayerResponse } from "@server/@types/api"
-import dayjs from "dayjs"
 import isEmpty from "lodash/isEmpty"
 import { IconType } from "react-icons"
 import { FiUserX } from "react-icons/fi"
 
 export const HomeContainerActivePlayersSection: React.FC = () => {
   const { isInitialLoading, players } = useGetPlayers({
-    limit: 20,
+    limit: 100,
   })
 
   const { authenticatedUser } = useAuthenticatedUser()
-  const isAuthenticatedUser = (player_id: PlayerResponse["player_id"]) =>
-    player_id === authenticatedUser?.player_id
-
-  const rows = players?.map(
-    ({ last_active_at, username, player_id }, index) => [
-      index + 1,
-      <Typography
-        variant={TypographyVariant.Span}
-        weight={
-          isAuthenticatedUser(player_id)
-            ? TypographyWeight.Bold
-            : TypographyWeight.Normal
-        }
-      >
-        {username} {isAuthenticatedUser(player_id) && "(you)"}
-      </Typography>,
-      dayjs(last_active_at).fromNow(),
-    ],
-  )
+  const rows = mapPlayersToRows(authenticatedUser?.player_id, players)
 
   return (
     <Card className="min-w-[320px]" title="Active Players">
